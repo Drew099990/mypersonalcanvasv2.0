d3a64fdaad191cfc4a95037a60fae2accb6399c2
Andrew muyunda <andrewmmuyunda777@gmail.com>
2025-07-08 20:38:53 +0200

    making responsive

--- app/components/Nav/page.js
+++ app/components/Nav/page.js
@@ -8,7 +8,7 @@ const Menu = () => {
             <Link href="/about" className="font-bold  border-gray-600 border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65  opacity-30 " >About</Link>
             <Link href="/PaS" className="font-bold  border-gray-600 border-x-2 px-1  leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >Inventory </Link>
             <Link href="/blog" className="font-bold  border-gray-600 border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >blog</Link> 
-            <Link href="https://wa.me/+260777290515" className="font-bold  border-gray-600  border-x-2  px-2 leading-tight  opacity-50 bg-gradient-to-r from-green-900 to-[#474a47] hover:opacity-65 flex items-center gap-1" >hire <FaWhatsapp size={18} /></Link>
+            <Link href="https://wa.me/+260777290515" className="font-bold  border-gray-600  border-x-2  px-2 leading-tight  opacity-50 bg-gradient-to-r from-green-900 to-[#474a47] scale-90 hover:opacity-65 flex items-center gap-1" >hire༼ つ ◕_◕ ༽つme<FaWhatsapp size={18} /></Link>
           </div>  
   )
 }
