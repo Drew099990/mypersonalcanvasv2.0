d63a5a8fa689d22344c860d43aab21722c695886
Andrew muyunda <andrewmmuyunda777@gmail.com>
2025-07-08 19:58:03 +0200

    making responsive

--- app/components/Nav/page.js
+++ app/components/Nav/page.js
@@ -4,11 +4,11 @@ import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
 const Menu = () => {
   return (
- <div className="flex  gap-5 h-7 max-[55rem]:scale-75 min-sm:hidden ">
-            <Link href="/about" className="font-bold  border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65  opacity-30 " >About</Link>
-            <Link href="/PaS" className="font-bold  border-x-2 px-1  leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >Inventory </Link>
-            <Link href="/blog" className="font-bold  border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >blog</Link> 
-            <Link href="https://wa.me/+260777290515" className="font-bold  border-x-2  px-2 leading-tight  opacity-50 bg-gradient-to-r from-green-900 to-[#474a47] hover:opacity-65 flex items-center gap-1" >hire me <FaWhatsapp size={18} /></Link>
+ <div className="flex  gap-5 h-7 max-[55rem]:scale-75  border-gray-600 text-gray-300 ">
+            <Link href="/about" className="font-bold  border-gray-600 border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65  opacity-30 " >About</Link>
+            <Link href="/PaS" className="font-bold  border-gray-600 border-x-2 px-1  leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >Inventory </Link>
+            <Link href="/blog" className="font-bold  border-gray-600 border-x-2 px-1 leading-tight bg-gradient-to-r from-gray-500 hover:opacity-65 opacity-30 to-[#474a47]" >blog</Link> 
+            <Link href="https://wa.me/+260777290515" className="font-bold  border-gray-600  border-x-2  px-2 leading-tight  opacity-50 bg-gradient-to-r from-green-900 to-[#474a47] hover:opacity-65 flex items-center gap-1" >hire me <FaWhatsapp size={18} /></Link>
           </div>  
   )
 }
--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       
    
       <div
-        className="bg-[#242924] flex flex-col justify-center items-center  max-md:min-h-[70vh] min-sm:min-h-[80vh] bg-[url('data:image/svg+xml,%3Csvg%20width%3D%2742%27%20height%3D%2744%27%20viewBox%3D%270%200%2042%2044%27%20xmlns%3D%27http://www.w3.org/2000/svg%27%3E%3Cg%20id%3D%27Page-1%27%20fill%3D%27none%27%20fill-rule%3D%27evenodd%27%3E%3Cg%20id%3D%27brick-wall%27%20fill%3D%27%239C92AC%27%20fill-opacity%3D%270.4%27%3E%3Cpath%20d%3D%27M0%200h42v44H0V0zm1%201h40v20H1V1zM0%2023h20v20H0V23zm22%200h20v20H22V23z%27/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"
+        className="bg-[#242924] flex flex-col justify-center items-center  max-md:min-h-[81vh] min-md:min-h-[84vh] bg-[url('data:image/svg+xml,%3Csvg%20width%3D%2742%27%20height%3D%2744%27%20viewBox%3D%270%200%2042%2044%27%20xmlns%3D%27http://www.w3.org/2000/svg%27%3E%3Cg%20id%3D%27Page-1%27%20fill%3D%27none%27%20fill-rule%3D%27evenodd%27%3E%3Cg%20id%3D%27brick-wall%27%20fill%3D%27%239C92AC%27%20fill-opacity%3D%270.4%27%3E%3Cpath%20d%3D%27M0%200h42v44H0V0zm1%201h40v20H1V1zM0%2023h20v20H0V23zm22%200h20v20H22V23z%27/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')]"
       />
 
    <div>
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
           
           <div className="bg-[#D2D0D0FF]  text-black/90 flex flex-col justify-center items-center  flex-2/3 min-w-fit overflow-hidden">
-            <Link href="/" className="flex max-md:px-2 z-20 max-md:scale-90 max-md:opacity-90 max-md:border border-r-gray-700 items-center gap-3 bg-[#D2D0D0FF] w-fit rounded-2xl my-5 px-2">
+            <Link href="/" className="flex max-md:px-2 min-md:invisible z-20 max-md:scale-90 max-md:opacity-90 max-md:border border-gray-400 items-center gap-3 bg-[#D2D0D0FF] w-fit rounded-2xl my-5 px-2">
             <Image className="rounded-[50%]" src={img} width={30} height={30} alt="Sleepy Panda Logo" />
             <div>
               <h1 className="font-bold max-md:scale-75  leading-tight">sleepy</h1>
